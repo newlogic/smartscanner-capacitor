958c170175b03c5be46d8f38421a0eb301f71e6d
Reuben Mangubat <reuben@newlogic.com>
2021-09-13 20:19:10 +0800

    Fix missing SmartScannerPluginInterface for Capacitor v3 update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import { registerPlugin } from '@capacitor/core';
+import type { SmartScannerPluginInterface } from './definitions';
+
+const MySmartScannerPlugin = registerPlugin<SmartScannerPluginInterface>('MyCoolPlugin', {
+  web: () => import('./web').then(m => new m.SmartScannerPluginWeb()),
+});
+
 export * from './definitions';
-export * from './web';
+export { MySmartScannerPlugin };
