ed5491335298bdcdb12b08d2b9bc89a02c3c13bd
Reuben Mangubat <reuben@newlogic.com>
2022-08-31 11:45:08 +0800

    remove elctron import

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import type { SmartScannerPlugin } from "./definitions";
 const SmartScannerPlugin = registerPlugin<SmartScannerPlugin>(
   'SmartScanner',
   {
-    web: () => import("./web").then((m) => new m.SmartScannerPluginWeb()),
-    electron: () => import('./web').then(m => new m.SmartScannerPluginWeb())
+    web: () => import("./web").then((m) => new m.SmartScannerPluginWeb())
   }
 );
 
