0d8c9fe0c43842b858ab6c1a8a68cd4f34575334
rs2-davebanguilan <dave.banguilan@rs2.com>
2022-12-20 22:03:35 +0800

    Remove unused function

--- src/web.ts
+++ src/web.ts
@@ -1,15 +1,9 @@
 import { WebPlugin } from '@capacitor/core';
-import { SmartScannerPluginInterface } from './definitions';
+import { ScannerInputConfig, SmartScannerPluginInterface } from './definitions';
 
 export class SmartScannerPluginWeb extends WebPlugin implements SmartScannerPluginInterface {
-
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
-  }
   
-  async executeScanner(options: { options: {}, action: string }): Promise<void> {
-    console.log('executeScanner', options);
+  async executeScanner(_options?: ScannerInputConfig): Promise<void> {
     throw new Error('method not available in web');
   }
 }
